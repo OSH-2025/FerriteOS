b970d376524d8b24f556972da3daca31e4fd2d26
zds <zy937@mail.ustc.edu.cn>
2025-05-04 18:24:29 +0800

    添加了memstat.rs中的部分绑定

--- src/rust/src/mem/memstat.rs
+++ src/rust/src/mem/memstat.rs
@@ -27,3 +27,19 @@ const LOSCFG_BASE_CORE_TSK_LIMIT: usize = 64;
 
 /// extra 1 blocks is for extra temparary task
 pub const TASK_NUM: usize = LOSCFG_BASE_CORE_TSK_LIMIT + 1;
+
+unsafe extern "C" {
+    #[link_name = "OsMemstatTaskUsedInc"]
+    unsafe fn os_memstat_task_used_inc_wrapper(stat: &mut Memstat, used_size: u32, task_id: u32);
+
+    #[link_name = "OsMemstatTaskUsedDec"]
+    unsafe fn os_memstat_task_used_dec_wrapper(stat: &mut Memstat, used_size: u32, task_id: u32);
+}
+
+pub fn os_memstat_task_used_inc(stat: &mut Memstat, used_size: u32, task_id: u32) {
+    unsafe { os_memstat_task_used_inc_wrapper(stat, used_size, task_id) }
+}
+
+pub fn os_memstat_task_used_dec(stat: &mut Memstat, used_size: u32, task_id: u32) {
+    unsafe { os_memstat_task_used_dec_wrapper(stat, used_size, task_id) }
+}
