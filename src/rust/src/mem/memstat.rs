e62c574ebfe441315b49cd61c3127bcbe96dd8de
zds <zy937@mail.ustc.edu.cn>
2025-05-10 12:50:26 +0800

    调整了rust编译feature

--- src/rust/src/mem/memstat.rs
+++ src/rust/src/mem/memstat.rs
@@ -29,6 +29,14 @@ pub fn os_memstat_task_used_inc(stat: &mut Memstat, used_size: u32, task_id: u32
     );
     stat.mem_total_used += used_size;
     stat.mem_total_peak = u32::max(stat.mem_total_peak, stat.mem_total_used);
+    // unsafe {
+    //     dprintf(
+    //         b"mem used of task '%d': 0x%x, increase size: 0x%x\n\0" as *const u8,
+    //         task_id,
+    //         stat.task_memstats[record].mem_used,
+    //         used_size,
+    //     );
+    // }
 }
 
 pub fn os_memstat_task_used_dec(stat: &mut Memstat, used_size: u32, task_id: u32) {
@@ -46,6 +54,14 @@ pub fn os_memstat_task_used_dec(stat: &mut Memstat, used_size: u32, task_id: u32
     }
     stat.task_memstats[record].mem_used -= used_size;
     stat.mem_total_used -= used_size;
+    // unsafe {
+    //     dprintf(
+    //         b"mem used of task '%d': 0x%x, decrease size: 0x%x\n\0" as *const u8,
+    //         task_id,
+    //         stat.task_memstats[record].mem_used,
+    //         used_size,
+    //     );
+    // }
 }
 
 fn os_mem_task_usage(stat: &Memstat, task_id: u32) -> u32 {
@@ -76,6 +92,12 @@ fn os_mem_task_clear(stat: &mut Memstat, task_id: u32) {
     }
     stat.task_memstats[record].mem_used = 0;
     stat.task_memstats[record].mem_peak = 0;
+    // unsafe {
+    //     dprintf(
+    //         b"mem used of task '%d' is cleared\n\0" as *const u8,
+    //         task_id,
+    //     );
+    // }
 }
 
 pub fn os_memstat_task_clear(task_id: u32) {
--- src/rust/src/utils/printf.rs
+++ src/rust/src/utils/printf.rs
@@ -7,11 +7,6 @@ unsafe extern "C" {
 macro_rules! os_check_null_return {
     ($param:expr) => {
         if $param.is_null() {
-            // crate::utils::printf::dprintf(
-            //     b"Null pointer detected at %s:%d\n\0".as_ptr(),
-            //     file!().as_ptr(),
-            //     line!(),
-            // );
             crate::utils::printf::dprintf(b"Null pointer detected at\n\0".as_ptr());
             return;
         }
