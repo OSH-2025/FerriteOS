88ea079177f019a1f84425ce8799b60424332ad1
zds <zy937@mail.ustc.edu.cn>
2025-05-24 22:30:47 +0800

    添加了priority_queue_remove函数

--- src/rust/src/task/sched.rs
+++ src/rust/src/task/sched.rs
@@ -1,6 +1,8 @@
 use crate::utils::list::LinkedList;
 use core::sync::atomic::{AtomicU32, Ordering};
 
+use super::types::TaskCB;
+
 const OS_PRIORITY_QUEUE_NUM: usize = 32;
 const PRIQUEUE_PRIOR0_BIT: u32 = 0x8000_0000;
 
@@ -12,7 +14,7 @@ static PRI_QUEUE_BITMAP: AtomicU32 = AtomicU32::new(0);
 
 /// 初始化优先级队列
 #[unsafe(export_name = "OsPriQueueInit")]
-pub extern "C" fn pri_queue_init() {
+pub extern "C" fn priority_queue_init() {
     for priority in 0..OS_PRIORITY_QUEUE_NUM {
         LinkedList::init(&mut unsafe { PRI_QUEUE_LIST }[priority]);
     }
@@ -50,4 +52,22 @@ pub extern "C" fn priority_queue_insert_at_back(priqueue_item: &mut LinkedList, 
         &mut unsafe { PRI_QUEUE_LIST }[priority as usize],
         priqueue_item,
     );
+}
+
+/// 从优先级队列中移除任务节点
+#[unsafe(export_name = "OsPriQueueDequeue")]
+pub extern "C" fn priority_queue_remove(priqueue_item: &mut LinkedList) {
+    // 从链表中删除节点
+    LinkedList::remove(priqueue_item);
+
+    // 获取包含此节点的任务控制块
+    let run_task = TaskCB::from_pend_list(priqueue_item);
+
+    // 如果该优先级队列为空，原子更新位图
+    if LinkedList::is_empty(&mut unsafe { PRI_QUEUE_LIST }[run_task.priority as usize]) {
+        PRI_QUEUE_BITMAP.fetch_and(
+            !(PRIQUEUE_PRIOR0_BIT >> run_task.priority),
+            Ordering::Release,
+        );
+    }
 }
