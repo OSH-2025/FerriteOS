a018f1c68b9896998ad45c2da5dd3b5cb412b39f
zds <zy937@mail.ustc.edu.cn>
2025-06-05 10:50:42 +0800

    完成了内核核心模块的重构

--- src/rust/src/lib.rs
+++ src/rust/src/lib.rs
@@ -24,16 +24,3 @@ mod utils;
 pub extern "C" fn hello_rust() {
     println_release!("Hello Rust!");
 }
-
-// #[macro_export]
-// macro_rules! os_check_null_return {
-//     ($param:expr) => {
-//         if $param.is_null() {
-//             crate::utils::printf::dprintf(b"Null pointer detected at\n\0".as_ptr());
-//             return;
-//         }
-//     };
-// }
-
-// use core::ffi::c_void;
-// use core::fmt;
